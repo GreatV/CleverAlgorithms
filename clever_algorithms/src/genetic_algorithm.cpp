99c394408ecb911488c9bfbe5801f3036b2daf05
greatx <xinwang614@gmail.com>
2022-10-08 10:17:49 +0800

    rename demos to examples

--- clever_algorithms/src/genetic_algorithm.cpp
+++
@@ -1,6 +0,0 @@
-#include "genetic_algorithm.h"
-
-GeneticAlgorithm::GeneticAlgorithm(int populationSize, double mutationRate, double crossoverRate, int elitismCount)
-    : populationSize_(populationSize), mutationRate_(mutationRate), crossoverRate_(crossoverRate), elitismCount_(elitismCount)
-{
-}
